f763ed575c63d6b4c230f1f8f05c0d9866d3dc47
Lapinas <mrlapinas99@gmail.com>
2022-02-14 19:28:05 +0000

    Changes

---
+++ fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'cerulean'
+
+game {'gta5'}
+
+description 'XO1-loadingscreen'
+version '1.0.0'
+author 'Nss'
+
+files {
+    'UI.html',
+    'Styling/UI.css'
+}
+
+loadscreen {
+    'UI.html',
+}
+
+loadscreen_cursor 'yes'
